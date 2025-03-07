c764a8dfc66466acdd69f474ed63846ea5477453
nhu.vu <nhu.vu@lazada.com>
2025-03-07 17:20:48 +0700

    add Hw0205 to _hw0205 branch

---
+++ L02_DataTypeAndIO/src/Hw0205_Console.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hw0205_Console {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Read name: ");
+        String name = scanner.nextLine();
+        System.out.print("Read age: ");
+        int age = scanner.nextInt();
+        System.out.print("Read height: ");
+        float height = scanner.nextFloat();
+        System.out.println();
+        System.out.printf("Họ và tên: %s \nTuổi: %d \nChiều cao: %.2fm", name, age, height);
+
+        scanner.close();
+    }
+}
