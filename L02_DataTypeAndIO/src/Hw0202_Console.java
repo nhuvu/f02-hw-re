3c8ea84b4c5d07d78b290809141695fb87f5aeef
nhu.vu <nhu.vu@lazada.com>
2025-03-07 17:19:33 +0700

    add Hw0202 to _hw0202 branch

---
+++ L02_DataTypeAndIO/src/Hw0202_Console.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Hw0202_Console {
+    /*
+    °C = (°F - 32) ÷ 1.8
+    °F = °C * 1.8 + 32
+    * */
+    public static void main(String[] args) throws FileNotFoundException {
+        //F to C
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Read °F to convert to °C: ");
+        double fahrenheit = scanner.nextDouble();
+        System.out.printf("%f °F equals to: %.2f °C\n\n", fahrenheit, (fahrenheit - 32) / 1.8);
+
+        //C to F
+        System.out.print("Read °C to convert to °F: ");
+        double celsius = scanner.nextDouble();
+        System.out.printf("%f °C equals to: %.2f °F", celsius, (celsius * 1.8) + 32);
+
+        scanner.close();
+
+
+    }
+}
