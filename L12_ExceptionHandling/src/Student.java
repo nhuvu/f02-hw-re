37b3c95cd632a476ff5d805c5269172997833e52
Arale <nhu.vu48@gmail.com>
2025-03-22 11:02:25 +0700

    add exercise 12 & 13

---
+++ L12_ExceptionHandling/src/Student.java
@@ -0,0 +1,64 @@
+public class Student {
+    private String name;
+    private String studentNo;
+    private int age;
+    private double averagePoint;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAveragePoint() {
+        return averagePoint;
+    }
+
+    public void setAveragePoint(double averagePoint) {
+        this.averagePoint = averagePoint;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Student(String name, String studentNo, int age, double averagePoint, String phone) {
+        this.name = name;
+        this.studentNo = studentNo;
+        this.age = age;
+        this.averagePoint = averagePoint;
+        this.phone = phone;
+    }
+
+    public Student(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Sinh viên: " + this.name + "; MSSV: " + this.studentNo + "; Tuổi: " + this.age + "; Điểm trung bình: " + this.averagePoint;
+    }
+}
